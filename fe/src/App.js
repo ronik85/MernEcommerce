578d46963b9e389c6450a888bdac0ccc414210dc
Ronik <ronik.kalanouria@gmail.com>
2023-12-07 11:37:23 +0530

    Added Routing

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
-import HomePage from './Pages/HomePage';
+import React from "react";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignupPage from "./Pages/SignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <HomePage />
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ fe/src/Pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../features/Auth/components/Login'
+
+function LoginPage() {
+    return (
+        <>
+            <Login />
+        </>
+    )
+}
+
+export default LoginPage
